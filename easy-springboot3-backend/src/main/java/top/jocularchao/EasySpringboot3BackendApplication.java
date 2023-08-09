8511e423d6c1888a036f7331176505abb6175011
jocularchao <124356596+jocularchao@users.noreply.github.com>
2023-08-09 22:14:10 +0800

    项目初始创建

---
+++ easy-springboot3-backend/src/main/java/top/jocularchao/EasySpringboot3BackendApplication.java
@@ -0,0 +1,13 @@
+package top.jocularchao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasySpringboot3BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasySpringboot3BackendApplication.class, args);
+    }
+
+}
---
+++ easy-springboot3-backend/src/test/java/top/jocularchao/EasySpringboot3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jocularchao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySpringboot3BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
