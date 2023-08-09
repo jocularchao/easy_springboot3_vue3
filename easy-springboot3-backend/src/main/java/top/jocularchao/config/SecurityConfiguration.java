945cac9a4ec5a05a9d1b1498362ef0bc639d14c6
jocularchao <124356596+jocularchao@users.noreply.github.com>
2023-08-09 23:37:38 +0800

    Security配置类

--- easy-springboot3-backend/src/main/java/top/jocularchao/config/SecurityConfiguration.java
+++ easy-springboot3-backend/src/main/java/top/jocularchao/config/SecurityConfiguration.java
@@ -1,8 +1,27 @@
-package top.jocularchao.config;/**
+package top.jocularchao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
  * Create with IntelliJ IDEA.
  *
- * @author      JocularChao
- * @date        2023/8/9 22:37
- * @Description 
- */public class SecurityConfiguration {
+ * @author JocularChao
+ * @date 2023/8/9 22:37
+ * @Description
+ */
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(conf -> conf
+                        .requestMatchers("api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
