91dc25908e80ac5cdfd3bc9661f8874a1c0dec8a
jocularchao <124356596+jocularchao@users.noreply.github.com>
2023-08-09 23:37:26 +0800

    Security配置类

---
+++ easy-springboot3-backend/src/main/java/top/jocularchao/config/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package top.jocularchao.config;/**
+ * Create with IntelliJ IDEA.
+ *
+ * @author      JocularChao
+ * @date        2023/8/9 22:37
+ * @Description 
+ */public class SecurityConfiguration {
+}
